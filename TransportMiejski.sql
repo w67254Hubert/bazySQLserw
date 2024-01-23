89716aabebfaae1716ea31e076ccf6d06597352b
w67254Hubert <w67254@student.wsiz.edu.pl>
2024-01-23 20:32:04 +0100

    zapisz

---
+++ TransportMiejski.sql
@@ -0,0 +1,252 @@
+CREATE DATABASE TransportMiejski_w67254t
+GO
+USE TransportMiejski_w67254t
+GO
+-- Table: KupBilety
+CREATE TABLE KupBilety (
+    KupBiletyID int  NOT NULL IDENTITY,
+    PasazerID int  NOT NULL,
+    DataZakupu datetime  NOT NULL,
+    Cena smallmoney  NOT NULL,
+    BiletomatID int  NOT NULL,
+    CONSTRAINT KupBilety_pk PRIMARY KEY  (KupBiletyID)
+);
+
+-- Table: Kursy
+CREATE TABLE Kursy (
+    KursyID int  NOT NULL IDENTITY,
+    LiniaID int  NOT NULL,
+    PracownikID int  NOT NULL,
+    IDPojazdu int  NOT NULL,
+    GodzRospoczencia time(0)  NOT NULL,
+    GodzZakonczena time(0)  NOT NULL,
+    CONSTRAINT Kursy_pk PRIMARY KEY  (KursyID)
+);
+
+-- Table: Linia
+CREATE TABLE Linia (
+    LiniaID int  NOT NULL IDENTITY,
+    Linia int  NOT NULL,
+    CONSTRAINT Linia_pk PRIMARY KEY  (LiniaID)
+);
+
+-- Table: Pasazerowie
+CREATE TABLE Pasazerowie (
+    PasazerID int  NOT NULL IDENTITY,
+    Imie varchar(100)  NOT NULL,
+    Nazwisko varchar(200)  NOT NULL,
+    NumerTel int  NOT NULL,
+    Email varchar(255)  NOT NULL,
+    Pesel varchar(11)  NOT NULL,
+    CONSTRAINT Pasazerowie_pk PRIMARY KEY  (PasazerID)
+);
+
+-- Table: Pojazdy
+CREATE TABLE Pojazdy (
+    IDPojazdu int  NOT NULL IDENTITY,
+    Stan varchar(50)  NOT NULL,
+    MarkaModel varchar(300)  NOT NULL,
+    DostempneMiejsca int  NOT NULL,
+    NumerRejestracyjny varchar(8)  NOT NULL,
+    BiletomatID int  NOT NULL,
+    CONSTRAINT Pojazdy_pk PRIMARY KEY  (IDPojazdu)
+);
+
+-- Table: Pracownicy
+CREATE TABLE Pracownicy (
+    PracownikID int  NOT NULL IDENTITY,
+    StanowiskoID int  NOT NULL,
+    Imie varchar(70)  NOT NULL,
+    Nazwisko varchar(70)  NOT NULL,
+    Pesel varchar(11)  NOT NULL,
+    dataZatrudnienia date  NOT NULL,
+    CONSTRAINT Pracownicy_pk PRIMARY KEY  (PracownikID)
+);
+
+-- Table: Przystanki
+CREATE TABLE Przystanki (
+    PrzystanekID int  NOT NULL IDENTITY,
+    NazwaPrzystanku varchar(200)  NOT NULL,
+    Lokalizacja varchar(300)  NOT NULL,
+    BiletomatID int  NOT NULL,
+    CONSTRAINT Przystanki_pk PRIMARY KEY  (PrzystanekID)
+);
+
+-- Table: Rodzajebiletu
+CREATE TABLE Rodzajebiletu (
+    RodzajebiletuID int  NOT NULL IDENTITY,
+    NazwaBiletu varchar(30)  NOT NULL,
+    CzasDzialania time(0)  NOT NULL,
+    CONSTRAINT Rodzajebiletu_pk PRIMARY KEY  (RodzajebiletuID)
+);
+
+-- Table: RozkladJazdy
+CREATE TABLE RozkladJazdy (
+    RozkladJazdyID int  NOT NULL IDENTITY,
+    LiniaID int  NOT NULL,
+    PrzystanekID int  NOT NULL,
+    KolejnoscNaTrasie int  NOT NULL,
+    godzinaPrzyjazdu time(0)  NOT NULL,
+    CONSTRAINT RozkladJazdy_pk PRIMARY KEY  (RozkladJazdyID)
+);
+
+-- Table: SkasowaneBilety
+CREATE TABLE SkasowaneBilety (
+    skasBiletID int  NOT NULL IDENTITY,
+    dataSkasowania datetime  NOT NULL,
+    ZakbiletyID int  NOT NULL,
+    KursyID int  NOT NULL,
+    CONSTRAINT SkasowaneBilety_pk PRIMARY KEY  (skasBiletID)
+);
+
+-- Table: Stanowiska
+CREATE TABLE Stanowiska (
+    StanowiskoID int  NOT NULL IDENTITY,
+    NazwaStanowiska varchar(100)  NOT NULL,
+    CONSTRAINT Stanowiska_pk PRIMARY KEY  (StanowiskoID)
+);
+
+-- Table: Uprawnienia
+CREATE TABLE Uprawnienia (
+    UprawnieniaID int  NOT NULL IDENTITY,
+    Koduprawnien int  NOT NULL,
+    OpisUprawnien varchar(200)  NOT NULL,
+    WymaganyWiek int  NOT NULL,
+    CONSTRAINT Uprawnienia_pk PRIMARY KEY  (UprawnieniaID)
+);
+
+-- Table: UprawnieniaKierowcow
+CREATE TABLE UprawnieniaKierowcow (
+    UprawnieniaID int  NOT NULL,
+    PracownikID int  NOT NULL,
+    doKiedy date  NOT NULL,
+    CONSTRAINT UprawnieniaKierowcow_pk PRIMARY KEY  (UprawnieniaID)
+);
+
+-- Table: ZmianaRozkladu
+CREATE TABLE ZmianaRozkladu (
+    ZminaRozkladuID int  NOT NULL IDENTITY,
+    LiniaID int  NOT NULL,
+    PrzysNaTras_ID int  NOT NULL,
+    KolejnoscNaTrasie int  NOT NULL,
+    GodzinaPrzyjazdu time(0)  NOT NULL,
+    odDaty date  NOT NULL,
+    CONSTRAINT ZmianaRozkladu_pk PRIMARY KEY  (ZminaRozkladuID)
+);
+
+-- Table: biletomaty
+CREATE TABLE biletomaty (
+    BiletomatID int  NOT NULL IDENTITY,
+    model varchar(100)  NOT NULL,
+    stan varchar(100)  NOT NULL,
+    CONSTRAINT biletomaty_pk PRIMARY KEY  (BiletomatID)
+);
+
+-- Table: cennik
+CREATE TABLE cennik (
+    biletID int  NOT NULL IDENTITY,
+    RodzajebiletuID int  NOT NULL,
+    zniszka varchar(50)  NOT NULL,
+    Cena smallmoney  NOT NULL,
+    CONSTRAINT cennik_pk PRIMARY KEY  (biletID)
+);
+
+-- Table: kierowcy
+CREATE TABLE kierowcy (
+    PracownikID int  NOT NULL,
+    StanowiskoID int  NOT NULL DEFAULT 2,
+    doKiedyBadania date  NOT NULL,
+    CONSTRAINT kierowcy_pk PRIMARY KEY  (PracownikID)
+);
+
+-- foreign keys
+-- Reference: Autobusy_biletomat (table: Pojazdy)
+ALTER TABLE Pojazdy ADD CONSTRAINT Autobusy_biletomat
+    FOREIGN KEY (BiletomatID)
+    REFERENCES biletomaty (BiletomatID);
+
+-- Reference: Bilety_Pasazerowie (table: KupBilety)
+ALTER TABLE KupBilety ADD CONSTRAINT Bilety_Pasazerowie
+    FOREIGN KEY (PasazerID)
+    REFERENCES Pasazerowie (PasazerID);
+
+-- Reference: Kursy_Autobusy (table: Kursy)
+ALTER TABLE Kursy ADD CONSTRAINT Kursy_Autobusy
+    FOREIGN KEY (IDPojazdu)
+    REFERENCES Pojazdy (IDPojazdu);
+
+-- Reference: Kursy_Trasa (table: Kursy)
+ALTER TABLE Kursy ADD CONSTRAINT Kursy_Trasa
+    FOREIGN KEY (LiniaID)
+    REFERENCES Linia (LiniaID);
+
+-- Reference: Kursy_kierowcy (table: Kursy)
+ALTER TABLE Kursy ADD CONSTRAINT Kursy_kierowcy
+    FOREIGN KEY (PracownikID)
+    REFERENCES kierowcy (PracownikID);
+
+-- Reference: Pracownicy_Stanowiska (table: Pracownicy)
+ALTER TABLE Pracownicy ADD CONSTRAINT Pracownicy_Stanowiska
+    FOREIGN KEY (StanowiskoID)
+    REFERENCES Stanowiska (StanowiskoID);
+
+-- Reference: PrzystanekNaTrasie_Przystanki (table: RozkladJazdy)
+ALTER TABLE RozkladJazdy ADD CONSTRAINT PrzystanekNaTrasie_Przystanki
+    FOREIGN KEY (PrzystanekID)
+    REFERENCES Przystanki (PrzystanekID);
+
+-- Reference: PrzystanekNaTrasie_Trasa (table: RozkladJazdy)
+ALTER TABLE RozkladJazdy ADD CONSTRAINT PrzystanekNaTrasie_Trasa
+    FOREIGN KEY (LiniaID)
+    REFERENCES Linia (LiniaID);
+
+-- Reference: Przystanki_biletomaty (table: Przystanki)
+ALTER TABLE Przystanki ADD CONSTRAINT Przystanki_biletomaty
+    FOREIGN KEY (BiletomatID)
+    REFERENCES biletomaty (BiletomatID);
+
+-- Reference: SkasowaneBilety_Kursy (table: SkasowaneBilety)
+ALTER TABLE SkasowaneBilety ADD CONSTRAINT SkasowaneBilety_Kursy
+    FOREIGN KEY (KursyID)
+    REFERENCES Kursy (KursyID);
+
+-- Reference: SkasowaneBilety_ZakBilety (table: SkasowaneBilety)
+ALTER TABLE SkasowaneBilety ADD CONSTRAINT SkasowaneBilety_ZakBilety
+    FOREIGN KEY (ZakbiletyID)
+    REFERENCES KupBilety (KupBiletyID);
+
+-- Reference: UprawnieniaKierowcow_Pracownicy (table: UprawnieniaKierowcow)
+ALTER TABLE UprawnieniaKierowcow ADD CONSTRAINT UprawnieniaKierowcow_Pracownicy
+    FOREIGN KEY (PracownikID)
+    REFERENCES Pracownicy (PracownikID);
+
+-- Reference: UprawnieniaKierowcow_Uprawnienia (table: UprawnieniaKierowcow)
+ALTER TABLE UprawnieniaKierowcow ADD CONSTRAINT UprawnieniaKierowcow_Uprawnienia
+    FOREIGN KEY (UprawnieniaID)
+    REFERENCES Uprawnienia (UprawnieniaID);
+
+-- Reference: ZakBilety_biletomat (table: KupBilety)
+ALTER TABLE KupBilety ADD CONSTRAINT ZakBilety_biletomat
+    FOREIGN KEY (BiletomatID)
+    REFERENCES biletomaty (BiletomatID);
+
+-- Reference: cennik_Rodzajebiletu (table: cennik)
+ALTER TABLE cennik ADD CONSTRAINT cennik_Rodzajebiletu
+    FOREIGN KEY (RodzajebiletuID)
+    REFERENCES Rodzajebiletu (RodzajebiletuID);
+
+-- Reference: kierowcy_Pracownicy (table: kierowcy)
+ALTER TABLE kierowcy ADD CONSTRAINT kierowcy_Pracownicy
+    FOREIGN KEY (PracownikID)
+    REFERENCES Pracownicy (PracownikID);
+
+-- Reference: zmianyWprzystankach_PrzystanekNaTrasie (table: ZmianaRozkladu)
+ALTER TABLE ZmianaRozkladu ADD CONSTRAINT zmianyWprzystankach_PrzystanekNaTrasie
+    FOREIGN KEY (PrzysNaTras_ID)
+    REFERENCES RozkladJazdy (RozkladJazdyID);
+
+-- Reference: zmianyWprzystankach_Trasa (table: ZmianaRozkladu)
+ALTER TABLE ZmianaRozkladu ADD CONSTRAINT zmianyWprzystankach_Trasa
+    FOREIGN KEY (LiniaID)
+    REFERENCES Linia (LiniaID);
+
---
+++ zapytania.sql
@@ -0,0 +1,219 @@
+1. To zapytanie pokaże nam, po jakich przystankach i w jakich godzinach przejeżdża dana linia po przystankach, jak i kolejność tych przystanków.}
+
+    SELECT * FROM RozkladJazdy WHERE LiniaID = 1 order by KolejnoscNaTrasie
+
+2. Podane poniżej zapytanie pokazuje nam 5 następnych przystanków z pominięciem 2 pierwszych}
+    SELECT * FROM RozkladJazdy where LiniaID=1
+    ORDER BY KolejnoscNaTrasie
+    OFFSET 2 rows
+    FETCH next 5 rows only
+
+3. Wynikiem poniższego zapytania będzie harmonogram jazdy na danym przystanku.}
+
+    SELECT LiniaID, godzinaPrzyjazdu FROM RozkladJazdy WHERE PrzystanekID
+    
+    = 1 order by godzinaPrzyjazdu 
+
+4 Zapytanie wybiera wszystkie linie zawarte w rozkładzie jazdy}
+
+    SELECT DISTINCT LiniaID FROM RozkladJazdy;
+
+5. Zapytanie zlicza ilość odwiedzonych przez daną linie przystanków podczas przejazdu.
+
+    SELECT LiniaID, COUNT(*) AS IloscPrzys FROM RozkladJazdy 
+    
+    GROUP BY LiniaID
+6. Podanie poniżej zapytanie poda nam ile w sumie i ile średnio poszczególny klient wydał na bilety}
+
+    SELECT PasazerID, 
+
+    sUM(Cena) OVER (PARTITION BY PasazerID) AS MAXWydatki,
+       
+    AVG(Cena) OVER (PARTITION BY PasazerID) AS srCenaBil
+       
+    FROM KupBilety;
+
+7. Zapytanie pokaże nam wszystkie imiona i nazwiska zawarte w bazie}
+
+    SELECT Imie,Nazwisko FROM Pracownicy UNION
+    SELECT Imie,Nazwisko FROM Pasazerowie;
+
+Zapytania zawierające JOIN
+
+1. Za pomocą przedstawionego złączenia tablic dowiadujemy się imiona i nazwiska osób które kupiły bilet}
+    
+    SELECT KupBilety.KupBiletyID, KupBilety.Cena, KupBilety.DataZakupu, Pasazerowie.Imie+' '+Pasazerowie.Nazwisko as pasarzer
+    
+    FROM KupBilety JOIN Pasazerowie ON KupBilety.PasazerID = Pasazerowie.PasazerID
+
+2. Wynikiem zapytania będzie wyświetlenie, przez jakie przystanki i w jakich godzinach przejedzie podana linia.}
+    
+    SELECT RozkladJazdy.godzinaPrzyjazdu, Przystanki.NazwaPrzystanku, Linia.Linia
+    
+    FROM RozkladJazdy 
+    
+    JOIN Przystanki ON RozkladJazdy.PrzystanekID = Przystanki.PrzystanekID
+    
+    JOIN Linia ON RozkladJazdy.LiniaID = Linia.LiniaID
+    
+    WHERE linia=2
+    
+    ORDER BY KolejnoscNaTrasie
+
+3. Wynikiem zapytania będzie pokazanie jakie linie i w jakich godzinach będą przyjeżdżać na przystanek o podanej nazwie}
+    
+    SELECT R.godzinaPrzyjazdu, P.NazwaPrzystanku, Linia.Linia 
+    
+    FROM RozkladJazdy AS R
+    
+    JOIN Przystanki as P ON R.PrzystanekID = P.PrzystanekID
+    
+    JOIN Linia ON R.LiniaID = Linia.LiniaID
+    
+    WHERE NazwaPrzystanku='NazwaPRzystanku'
+    
+    ORDER BY godzinaPrzyjazdu
+
+4. Wynikiem zapytania będzie Uśredniona cena poszczególnych rodzajów biletów}
+
+    Select R.NazwaBiletu, avg(C.Cena) as średCena
+    FROM Rodzajebiletu as R join cennik as C on R.RodzajebiletuID=C.RodzajebiletuID
+    group by R.NazwaBiletu
+    order by średCena
+
+5.Podzapytanie podaje średnią liczbę lat przepracowanych przez pracowników na danym stanowisku}
+
+    SELECT S.NazwaStanowiska, AVG(DATEDIFF(YEAR, P.dataZatrudnienia, GETDATE())) AS srdLiczLatZatr
+    
+    FROM Stanowiska as S JOIN Pracownicy as P ON S.StanowiskoID = P.StanowiskoID
+
+    GROUP BY S.NazwaStanowiska;
+
+6. Poniższe zapytanie pokaże nam ID skasowanego biletu razem z osobą, która ten bilet kupiła}
+
+    Select s.skasBiletID, Imie, Nazwisko
+    
+    from SkasowaneBilety as S
+    
+    join KupBilety on S.ZakbiletyID=KupBilety.KupBiletyID 
+    
+    join Pasazerowie as P on KupBilety.PasazerID=P.PasazerID
+
+
+Zapytania zawierające LEFT, Right, Cross JOIN
+
+1. Zapytanie podane poniżej poda nam Różnice pomiędzy najdroższym i najtańszym biletem}
+
+    Select top 1 (C.Cena-C1.Cena) as ruznica
+    
+    from cennik as C cross join cennik as C1
+    
+    order by 1 desc
+
+2. Za pomocą podanego poniżej zapytania zobaczymy na jakich stanowiskach pracują pracownicy}
+
+    SELECT Pracownicy.Imie, Pracownicy.Nazwisko, Stanowiska.NazwaStanowiska
+    
+    FROM Pracownicy LEFT JOIN Stanowiska ON Pracownicy.StanowiskoID = Stanowiska.StanowiskoID;
+
+3. Podane poniżej zapytanie przedstawi nam, jaki autobus został przypisany do danego kursu.}
+
+    SELECT Kursy.KursyID, Pojazdy.MarkaModel, Pojazdy.DostempneMiejsca
+    
+    FROM Kursy LEFT JOIN Pojazdy ON Kursy.IDPojazdu = Pojazdy.IDPojazdu
+
+4. Podane zapytanie oceni nam czy cena zakupu biletu jest przystępna.}
+
+    SELECT P.imie,P.Nazwisko, Cena,
+    
+    CASE WHEN Cena < 10 THEN 'Tanio' WHEN Cena >= 10 AND Cena < 20 THEN 'Przyzwoicie' ELSE 'Drogo' END AS kategorie
+    
+    FROM KupBilety as K left join Pasazerowie as P on k.PasazerID=p.PasazerID
+
+zapytań z podzapytaniem w części SELECT
+
+1. Zapytanie pokaże nam listę pasażerów razem z ilością biletów, które zakupili}
+
+    SELECT Imie, Nazwisko, 
+    
+    (SELECT COUNT(*) FROM KupBilety WHERE KupBilety.PasazerID = Pasazerowie.PasazerID) AS TotalTickets
+    
+    FROM Pasazerowie;
+
+2. Poniższe zapytanie pokazuje nam ile autobusów będzie odjeżdżać z danego przystanku.}
+
+    SELECT NazwaPrzystanku, (
+    
+    SELECT COUNT(*) FROM RozkladJazdy WHERE RozkladJazdy.PrzystanekID = Przystanki.PrzystanekID) AS liczOdjazdy
+    
+    FROM Przystanki;
+
+3. Poniższe zapytanie poda nam stan pojazdu na danym kursie dzięki czemu możemy się dowiedzieć czy kurs się odbędzie}
+
+    SELECT KursyID, GodzRospoczencia, GodzZakonczena, (
+    
+    SELECT Stan FROM Pojazdy WHERE Pojazdy.IDPojazdu = Kursy.IDPojazdu) AS BusModel
+    
+    FROM Kursy;
+
+zapytań z podzapytaniem w części FROM
+
+1. Zapytanie pokazuje ile wydali poszczególni pasażerowie na bilety}
+
+    SELECT Pasazerowie.Imie, Pasazerowie.Nazwisko, ileWydał
+    
+    FROM Pasazerowie JOIN (
+    SELECT PasazerID, SUM(Cena) AS ileWydał FROM KupBilety GROUP BY PasazerID) AS E
+
+    ON Pasazerowie.PasazerID = E.PasazerID;
+
+2. Zapytanie pokazuje do ilu kursów przypisani są poszczególni pracownicy wymienieni z imienia i nazwiska}
+
+    SELECT Pracownicy.Imie, Pracownicy.Nazwisko, ilekursow
+    
+    FROM Pracownicy JOIN (
+    
+    SELECT PracownikID, COUNT(*) AS ilekursow FROM Kursy GROUP BY PracownikID) AS EK
+    
+    ON Pracownicy.PracownikID = EK.PracownikID;
+
+3. Poniższe zapytanie pokaże nam, jaka jest średnia cen biletów kupionych w autobusach}
+
+    SELECT P.NumerRejestracyjny,P.MarkaModel, srCena
+    
+    FROM Pojazdy as P JOIN (
+    SELECT BiletomatID, AVG(cena) AS srCena FROM kupbilety GROUP BY BiletomatID
+    ) AS B ON P.BiletomatID = B.BiletomatID;
+
+Zapytań z podzapytaniem w części WHERE
+
+1. Poniższe zapytanie zlicza liczbę przystanków, po których przejeżdża linia. Podaje tylko te, które mają powyżej 10 przystanków.}
+
+    SELECT LiniaID, COUNT(*) AS IloscPrzystankow
+    
+    FROM RozkladJazdy
+    
+    WHERE LiniaID IN (SELECT DISTINCT LiniaID FROM RozkladJazdy WHERE 
+    KolejnoscNaTrasie > 1)
+    
+    GROUP BY LiniaID
+
+Identyczny efekt można osiągnąć dodająca zamiast podzapytania w części WHERE dodać HAVING z następującym warunkiem:}
+
+    HAVING COUNT(*) >5;
+
+2. Zapytanie pokaże nam kursy, na których będzie jechały autobusy Volvo700}
+    
+    SELECT KursyID, GodzRospoczencia, GodzZakonczena
+    
+    FROM Kursy
+    
+    WHERE IDPojazdu IN (SELECT IDPojazdu FROM Pojazdy WHERE MarkaModel = 'Volvo7000');
+
+3. Zapytanie pokazuje nam jakie przystanki mają aktywny biletomat}
+
+    SELECT NazwaPrzystanku
+    
+    FROM Przystanki
+    
+    WHERE BiletomatID IN (SELECT BiletomatID FROM biletomaty WHERE stan = 'Aktywny')
